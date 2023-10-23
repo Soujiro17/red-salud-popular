55353a15b1a93f79ed97b0dfb7fc32abfd50873a
Soujiro17 <vicente_tomi@hotmail.com>
2023-10-23 00:10:36 -0300

    loader

--- src/components/Spinner/index.jsx
+++ src/components/Spinner/index.jsx
@@ -3,7 +3,9 @@ import styles from "./spinner.module.css";
 export function Spinner() {
   return (
     <div className={styles.loader_container}>
-      <span className={styles.loader} />
+      <div className={styles.loader_wrapper}>
+        <span className={styles.loader} />
+      </div>
     </div>
   );
 }
