939531c5180b58c47612df52de879bd004645b6c
Soujiro17 <vicente_tomi@hotmail.com>
2023-10-25 18:52:41 -0300

    login page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,25 @@
 "use client";
 
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { FormGroup } from "@/components/FormGroup";
 import styles from "./page.module.css";
 import { Logo } from "@/components/Logo";
 import { Button } from "@/components/Button";
+import { Spinner } from "@/components/Spinner";
 
 export default function Inicio() {
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
 
   const onSubmit = (e) => {
     e.preventDefault();
-    router.push("/dashboard");
+
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+      router.push("/dashboard");
+    }, 3000);
   };
 
   return (
@@ -19,15 +27,21 @@ export default function Inicio() {
       <div className={styles.login_container}>
         <Logo height={80} width={200} />
         <form onSubmit={onSubmit} className={styles.login_form}>
-          <FormGroup label="RUT" placeholder="RUT" />
-          <FormGroup
-            type="password"
-            label="Contraseña"
-            placeholder="Contraseña"
-          />
-          <Button type="submit" schema="info">
-            Iniciar sesión
-          </Button>
+          {isLoading ? (
+            <Spinner />
+          ) : (
+            <>
+              <FormGroup label="RUT" placeholder="RUT" />
+              <FormGroup
+                type="password"
+                label="Contraseña"
+                placeholder="Contraseña"
+              />
+              <Button type="submit" schema="info">
+                Iniciar sesión
+              </Button>
+            </>
+          )}
         </form>
       </div>
     </main>
